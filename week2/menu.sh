fb2f5339bfabee93d39652d61e633696a55278fd
kkaponay <kkaponay@our.ecu.edu.au>
2021-07-25 13:08:03 +0000

    Add Week 2 scripts to portfolio

---
+++ week2/menu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./scripts/portfolio/week2/passwordCheck.sh
+
+read
+
+if $pass_var=0
+    then
+    echo "Select a folder" "1. Create a folder"
+        1. Create a folder
+        2. Copy a folder
+        3. Seta password
+
+fi
---
+++ week2/passwordCheck.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Write a script named 'passwordCheck.sh' that accomplishes the following:
+    # 1. Ask the user to type a secret password (hide that input).
+    # 2. Check the user’s password against the hash stored in 'secret.txt'.
+    # 3. If the user’s password is correct, print “Access Granted” and quit with the exit code 0.
+    # 4. If the user’s password is incorrect print “Access Denied” and quit with the exit code 1.
+
+read -sp 'Please enter password ' pass_var
+
+echo
+hash_var="$(echo $pass_var | sha256sum)" 
+passhashfile="./secret.txt"
+
+while ifs= read -r line;
+do
+    if [ "$hash_var" == "$line" ]
+    then
+        echo "Access Granted"
+        echo 0
+
+    else
+        echo "Access Denied"
+        echo 1
+
+    fi 
+done <"$passhashfile"
+exit
