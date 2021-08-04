fe1fbf77ca53baeaee0b935e52c43aeaab8b2103
kkaponay <kkaponay@our.ecu.edu.au>
2021-08-04 06:32:10 +0000

    Week 1  Updated Scripts

--- week1/second.sh
+++ week1/second.sh
@@ -1,9 +1,12 @@
+#!/bin/bash 
 
-       #!/bin/bash 
+# second.sh script, week1
+# script to command numeric values
+# execute the script with own name e.g ./second.sh kelly
+# $1 will return as kelly
       
 echo "Hi there!" 
       
 echo "It's good to see you $1!" 
       
 exit 0 
-     
